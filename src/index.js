6dd337f3d090a94b76ede7344a007613bfb7e3ae
Jenny Alexandra Montoya Cortés <30633488+jennymontoya1001@users.noreply.github.com>
2021-07-28 19:37:48 -0500

    Validación Formik & Yup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Formulario} from './components/Formulario';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+ReactDOM.render(
+    <Formulario />,
+  document.getElementById('root')
+);
+
